9d15dedc724446e12f6e1d9f97471708fc7ae994
Juozas Baliuka <baliuka@mwm.lt>
2002-07-03 18:25:50 +0000

    Initial revision

---
+++ src/proxy/net/sf/cglib/proxy/Factory.java
@@ -0,0 +1,19 @@
+/*
+ * Factory.java
+ *
+ * Created on Ðeðtadienis, 2002, Geguþës 25, 19.40
+ */
+
+package net.sf.cglib.proxy;
+
+/**
+ *
+ * @author  user
+ */
+  public interface Factory{
+      
+       public Object newInstance( MethodInterceptor ih );    
+    }
+   
+    
+
