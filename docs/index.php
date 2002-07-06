00cf40d93e19ab86916f101d163fd31a4f98a0e8
Juozas Baliuka <baliuka@mwm.lt>
2002-07-06 10:20:37 +0000

    started documentation

---
+++ docs/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<HTML>
+  <HEAD>
+    <TITLE></TITLE>
+  </HEAD>
+  <BODY>
+  <A href="http://sourceforge.net"> <IMG src="http://sourceforge.net/sflogo.php?group_id=56933&amp;type=5" width="210" height="62" border="0" alt="SourceForge Logo"></A> 
+  <br>
+   <center><a href='api/index.html'> Javadoc </a><center>
+
+  </BODY>
+</HTML>
