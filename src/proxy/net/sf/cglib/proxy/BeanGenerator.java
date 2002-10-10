25be848bcc2d592506b76cabe27a014523e4995b
baliuka <baliuka>
2002-10-10 18:23:59 +0000

    added some stuff for BeanInfo generation

--- src/proxy/net/sf/cglib/proxy/BeanGenerator.java
+++ src/proxy/net/sf/cglib/proxy/BeanGenerator.java
@@ -64,154 +64,206 @@ import java.util.*;
  */
 public class BeanGenerator extends ClassLoader implements ClassFileConstants {
     
+    public static final String CLASS_NAME = "net.sf.cglib.proxy.Bean$Generated$By$CGLIB";
+    
+    private static final String BEAN_INFO_CLASS_NAME = CLASS_NAME + "BeanInfo";
+    
     private List properties    = new ArrayList();
     private Set   names        = new HashSet();
     private List  methods      = new ArrayList();
-    private Map cache          = new HashMap();
-   
-     /** Creates a new instance of BeanUtils */
+    private Class beanInfo;
+    private Class result;
+    
+    
+    /** Creates a new instance of BeanUtils */
     public BeanGenerator( ) {
         
     }
-   
+    
     /** Creates a new instance of BeanUtils */
     public BeanGenerator( ClassLoader parent ) {
         super(parent);
     }
-    
-    public void clear(){
-        properties.clear();
-        names.clear();
-        methods.clear();
-        cache.clear();
-    }
-    
-    private void generateMethod( ClassGen cg, String name,
-    java.lang.reflect.Method mtd ){
+    // can be implemented without code generation
+    public static class Info extends java.beans.SimpleBeanInfo {
         
-        ConstantPoolGen cp = cg.getConstantPool();
-        InstructionList il = new InstructionList();
-        Type params[] = ClassFileUtils.toType(mtd.getParameterTypes());
-        Type ret      = ClassFileUtils.toType(mtd.getReturnType());
-        MethodGen mg  = new MethodGen( ACC_PUBLIC,
-        ret, params , null, name, cg.getClassName(), il, cp);
+        private BeanGenerator generator ;
         
-        Class [] exeptions = mtd.getExceptionTypes();
-        
-        for( int i = 0 ; i< exeptions.length; i++ ){
-            mg.addException( exeptions[i].getName() );
+        public Info(){
+          generator = (BeanGenerator)getClass().getClassLoader();   
         }
-        int index = 1;
-        for( int i = 0; i< params.length; i++){
-            index = ClassFileUtils.loadArg(il,params[i], index );
-        }
-        il.append( new INVOKESTATIC(
-        cp.addMethodref( mtd.getDeclaringClass().getName(),
-        name, mg.getSignature() ) ) );
-        il.append( ClassFileUtils.newReturn(ret) );
-        cg.addMethod(  ClassFileUtils.getMethod( mg ) );
-        if( mtd.getName().startsWith("IE") ){
-         System.out.println( ClassFileUtils.getMethod( mg ).getCode() );
-        }
+       
         
-    }
-    private void generateProperty(ClassGen cg, String name, Class  type ){
-        
-        ConstantPoolGen cp = cg.getConstantPool();
-        InstructionList il = new InstructionList();
-        
-        Type propType = ClassFileUtils.toType(type);
-        FieldGen fg = new FieldGen( ACC_PRIVATE , propType , name, cp );
-        cg.addField(fg.getField());
-        
-        char propName[] = name.toCharArray();
-        propName[0] = Character.toUpperCase( propName[0] );
-        
-        MethodGen getter = new MethodGen( ACC_PUBLIC ,  propType , new Type[0],
-        null, type == Boolean.TYPE ? "is" : "get" + new String(propName),
-        cg.getClassName(), il,  cp);
-        il.append( new ALOAD(0)  );
-        il.append( new GETFIELD(
-        cp.addFieldref(cg.getClassName(), name,
-        propType.getSignature()) )  );
-        il.append( ClassFileUtils.newReturn(propType) );
-        
-        cg.addMethod(  ClassFileUtils.getMethod(getter) );
-        
-        il.dispose();
-        
-        MethodGen setter = new MethodGen( ACC_PUBLIC , Type.VOID , new Type[]{propType},
-        null, "set" + new String(propName), cg.getClassName(), il,  cp);
-        
-        il.append( new ALOAD(0) );
-        ClassFileUtils.loadArg( il, propType, 1);
-        il.append( new PUTFIELD(
-        cp.addFieldref(cg.getClassName(), name,
-        propType.getSignature()) )  );
-        
-        il.append( new RETURN() );
-        
-        cg.addMethod( ClassFileUtils.getMethod(setter) );
-        
-    }
-    
-    public Class generate(){
-        
-        String key = getKey();
-        Class result = (Class)cache.get(key);
-        if( result != null ){
-            return result;
-        }
-        
-        ClassGen cg = new ClassGen( key, Object.class.getName(),SOURCE_FILE,
-        ACC_PUBLIC  , null );
-        
-        cg.addEmptyConstructor( ACC_PUBLIC );
-        
-        for( Iterator i = properties.iterator(); i.hasNext();  ){
-            BeanProperty prop = (BeanProperty)i.next();
-            generateProperty( cg, prop.name, prop.type );
-            
-        }
-        
-        for( Iterator i = methods.iterator(); i.hasNext();  ){
-            BeanMethod m = (BeanMethod)i.next();
-            generateMethod( cg, m.name, m.method );
-            
-        }
-        
-        byte b[] = cg.getJavaClass().getBytes();
-        result = defineClass( cg.getClassName(), b, 0, b.length );
-        
-        cache.put( cg.getClassName() , result);
-        
-        return result;
+    } 
         
         
-    }
-    
-    public void addMethods( Class clasz ){
-        
-        java.lang.reflect.Method methods[] = clasz.getMethods();
-        
-        for( int i = 0; i< methods.length; i++ ){
-            
-            int mod = methods[i].getModifiers();
-            
-            if( java.lang.reflect.Modifier.isStatic(mod) &&
-            java.lang.reflect.Modifier.isPublic(mod ) ){
+        protected Class findClass(String name)
+        throws ClassNotFoundException{
+           
+            if( name.equals(BEAN_INFO_CLASS_NAME) ){
                 
-                this.methods.add( new BeanGenerator.BeanMethod(
-                                   methods[i].getName(),
-                                   methods[i]) 
-                          );
+                if( result != null ){
+                    
+                    if( beanInfo != null ){
+                        return beanInfo;
+                    }
+                    
+                 java.io.InputStream is = getParent().
+                                          getResourceAsStream(
+                                          Info.class.getName().replace('.','/') + ".class"   
+                    );
+                 if( is == null ){
+                     throw new ClassNotFoundException("Can't find " + name);
+                 }
+                 
+                java.io.ByteArrayOutputStream out = new  java.io.ByteArrayOutputStream();
+                
+                try{ 
+                 while(is.available() > 0){
+                   out.write( is.read() );
+                 }
+                }catch(java.io.IOException ioe){
+                    ioe.printStackTrace();
+                     throw new ClassNotFoundException( ioe.getMessage() );               
+                }
+                 byte [] b  = out.toByteArray();
+                 beanInfo =  defineClass(b,0,b.length);
+                
+                 
+                  return  beanInfo;
+                    
+                }else {
+                  throw new ClassNotFoundException();
+                }
+            }else if( name.equals( CLASS_NAME )){
+                 return generate();
+            }
+            return super.findClass(name);
+        }
+        
+        
+        
+        private void generateMethod( ClassGen cg, String name,
+        java.lang.reflect.Method mtd ){
+            
+            ConstantPoolGen cp = cg.getConstantPool();
+            InstructionList il = new InstructionList();
+            Type params[] = ClassFileUtils.toType(mtd.getParameterTypes());
+            Type ret      = ClassFileUtils.toType(mtd.getReturnType());
+            MethodGen mg  = new MethodGen( ACC_PUBLIC,
+            ret, params , null, name, cg.getClassName(), il, cp);
+            
+            Class [] exeptions = mtd.getExceptionTypes();
+            
+            for( int i = 0 ; i< exeptions.length; i++ ){
+                mg.addException( exeptions[i].getName() );
+            }
+            int index = 1;
+            for( int i = 0; i< params.length; i++){
+                index = ClassFileUtils.loadArg(il,params[i], index );
+            }
+            il.append( new INVOKESTATIC(
+            cp.addMethodref( mtd.getDeclaringClass().getName(),
+            name, mg.getSignature() ) ) );
+            il.append( ClassFileUtils.newReturn(ret) );
+            cg.addMethod(  ClassFileUtils.getMethod( mg ) );
+            
+        }
+        private void generateProperty(ClassGen cg, String name, Class  type ){
+            
+            ConstantPoolGen cp = cg.getConstantPool();
+            InstructionList il = new InstructionList();
+            
+            Type propType = ClassFileUtils.toType(type);
+            FieldGen fg = new FieldGen( ACC_PRIVATE , propType , name, cp );
+            cg.addField(fg.getField());
+            
+            char propName[] = name.toCharArray();
+            propName[0] = Character.toUpperCase( propName[0] );
+            
+            MethodGen getter = new MethodGen( ACC_PUBLIC ,  propType , new Type[0],
+            null, type == Boolean.TYPE ? "is" : "get" + new String(propName),
+            cg.getClassName(), il,  cp);
+            il.append( new ALOAD(0)  );
+            il.append( new GETFIELD(
+            cp.addFieldref(cg.getClassName(), name,
+            propType.getSignature()) )  );
+            il.append( ClassFileUtils.newReturn(propType) );
+            
+            cg.addMethod(  ClassFileUtils.getMethod(getter) );
+            
+            il.dispose();
+            
+            MethodGen setter = new MethodGen( ACC_PUBLIC , Type.VOID , new Type[]{propType},
+            null, "set" + new String(propName), cg.getClassName(), il,  cp);
+            
+            il.append( new ALOAD(0) );
+            ClassFileUtils.loadArg( il, propType, 1);
+            il.append( new PUTFIELD(
+            cp.addFieldref(cg.getClassName(), name,
+            propType.getSignature()) )  );
+            
+            il.append( new RETURN() );
+            
+            cg.addMethod( ClassFileUtils.getMethod(setter) );
+            
+        }
+        
+        public Class generate(){
+            
+            if( result != null ){
+                return result;
+            }
+            
+            ClassGen cg = new ClassGen( CLASS_NAME , Object.class.getName(),SOURCE_FILE,
+            ACC_PUBLIC  , null );
+            
+            cg.addEmptyConstructor( ACC_PUBLIC );
+            
+            for( Iterator i = properties.iterator(); i.hasNext();  ){
+                BeanProperty prop = (BeanProperty)i.next();
+                generateProperty( cg, prop.name, prop.type );
                 
             }
             
+            for( Iterator i = methods.iterator(); i.hasNext();  ){
+                BeanMethod m = (BeanMethod)i.next();
+                generateMethod( cg, m.name, m.method );
+                
+            }
+            
+            byte b[] = cg.getJavaClass().getBytes();
+            result = defineClass( cg.getClassName(), b, 0, b.length );
+
+            
+            return result;
+            
+            
         }
-    } 
         
-        public void addMethod( String name, java.lang.reflect.Method method ){
+        public BeanGenerator copyMethods( Class clasz ){
+            
+            java.lang.reflect.Method methods[] = clasz.getMethods();
+            
+            for( int i = 0; i< methods.length; i++ ){
+                
+                int mod = methods[i].getModifiers();
+                
+                if( java.lang.reflect.Modifier.isStatic(mod) &&
+                java.lang.reflect.Modifier.isPublic(mod ) ){
+                    
+                    this.methods.add( new BeanGenerator.BeanMethod(
+                    methods[i].getName(),
+                    methods[i])
+                    );
+                    
+                }
+                
+            }
+            return this;
+        }
+        
+        public BeanGenerator addMethod( String name, java.lang.reflect.Method method ){
             
             int mod = method.getModifiers();
             if( !java.lang.reflect.Modifier.isStatic(mod) ||
@@ -221,34 +273,36 @@ public class BeanGenerator extends ClassLoader implements ClassFileConstants {
             }
             
             methods.add( new BeanGenerator.BeanMethod(name,method) );
+            return this;
         }
         
-        public void addMethod(  java.lang.reflect.Method method ){
+        public BeanGenerator addMethod(  java.lang.reflect.Method method ){
             addMethod( null, method );
+            return this;
         }
         
         
-        public void addProperties( java.util.Map props ){
+        public BeanGenerator addProperties( java.util.Map props ){
             
             for( java.util.Iterator i = props.entrySet().iterator(); i.hasNext();  ){
                 Map.Entry entry = (Map.Entry)i.next();
                 addProperty((String)entry.getKey(),(Class)entry.getValue());
             }
-            
+            return this;
         }
-    
-         public void addProperties( java.beans.PropertyDescriptor[] descriptors ){
+        
+        public BeanGenerator addProperties( java.beans.PropertyDescriptor[] descriptors ){
             
             for( int i = 0; i < descriptors.length; i++  ){
                 addProperty(descriptors[i].getName(),
-                            descriptors[i].getPropertyType());
+                descriptors[i].getPropertyType());
             }
-            
+            return this;
         }
-    
         
         
-        public void addProperty( String name, Class type ){
+        
+        public BeanGenerator addProperty( String name, Class type ){
             
             if( names.contains(name) ){
                 throw new IllegalArgumentException("Dublicate property name " + name);
@@ -256,38 +310,7 @@ public class BeanGenerator extends ClassLoader implements ClassFileConstants {
             
             names.add( name );
             properties.add( new BeanProperty( name, type) );
-            
-        }
-        
-        private String toHex( String source ){
-            
-            StringBuffer sb = new StringBuffer();
-            char c[] = source.toCharArray();
-            for( int i = 0; i < c.length; i++   ){
-                sb.append( Integer.toHexString( (int)c[i]) );
-            }
-            return sb.toString();
-        }
-        
-        private String getKey(){
-            
-            StringBuffer sb = new StringBuffer();
-            for( Iterator i = properties.iterator(); i.hasNext();  ){
-                BeanProperty prop = (BeanProperty)i.next();
-                sb.append( prop.name + "$" + toHex(prop.type.getName()) );
-            }
-            for( Iterator i = methods.iterator(); i.hasNext();  ){
-                BeanMethod m = (BeanMethod)i.next();
-                sb.append(  m.name );
-                Class types[] =  m.method.getParameterTypes();
-                for( int ind = 0; ind < types.length; ind++   ){
-                    sb.append( "$" +  toHex( types[ind].getName() ) );
-                }
-                sb.append( "$" + toHex( m.method.getReturnType().getName() ) );
-                
-            }
-            
-            return sb.toString();
+            return this;
         }
         
         static class BeanProperty{
@@ -315,25 +338,34 @@ public class BeanGenerator extends ClassLoader implements ClassFileConstants {
         
         public static void main(String args[])throws Exception{
             
-            BeanGenerator bg = new BeanGenerator( );
+            for(int j = 0; j< 2 ;j++){
+                
+            Class cls = new BeanGenerator( ).
+            addProperty( "property", int.class ).
+            addMethod( BeanGenerator.class.getMethod("main",
+                                 new Class[]{ String[].class }) ).
+            copyMethods( java.lang.Math.class ).
+            generate();
             
-            bg.addProperty("property", int.class );
-            bg.addMethod( BeanGenerator.class.getMethod("main",
-            new Class[]{ String[].class }) );
-            bg.addMethods( java.lang.Math.class );
             
             
-            Class cls = bg.generate();
-            System.out.println( cls.getName() );
+            java.beans.BeanInfo info = java.beans.Introspector.getBeanInfo(cls);
+            
+            
+            info.getDefaultEventIndex() ;
+            
             Object obj = cls.newInstance();
             Object methods[] = cls.getMethods();
             
+            
+            
             for( int i = 0; i< methods.length; i++ ){
-                System.out.println( methods[i] );
+                //System.out.println( methods[i] );
             }
             
             
         }
         
+        }
         
     }
