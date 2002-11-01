545925f57cd50f253713a4a763adb69290a7264d
baliuka <baliuka>
2002-11-01 07:08:29 +0000

    added problematic classes from hibernate to cglib tests

---
+++ src/test/net/sf/cglib/proxy/D.java
@@ -0,0 +1,75 @@
+package net.sf.cglib.proxy;
+
+
+public class D {
+
+	private Long id;
+
+	private float amount;
+
+	/**
+
+	 * Returns the amount.
+
+	 * @return float
+
+	 */
+
+	public float getAmount() {
+
+		return amount;
+
+	}
+
+
+
+	/**
+
+	 * Returns the id.
+
+	 * @return long
+
+	 */
+
+	public Long getId() {
+
+		return id;
+
+	}
+
+
+
+	/**
+
+	 * Sets the amount.
+
+	 * @param amount The amount to set
+
+	 */
+
+	public void setAmount(float amount) {
+
+		this.amount = amount;
+
+	}
+
+
+
+	/**
+
+	 * Sets the id.
+
+	 * @param id The id to set
+
+	 */
+
+	public void setId(Long id) {
+
+		this.id = id;
+
+	}
+
+
+
+}
+
