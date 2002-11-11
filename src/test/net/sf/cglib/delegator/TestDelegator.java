1c1156baeebfa31ad59fdce523d1ca0031f68c77
herbyderby <herbyderby>
2002-11-11 20:44:55 +0000

    Added ClassLoader argument to makeDelegator methods.

--- src/test/net/sf/cglib/delegator/TestDelegator.java
+++ src/test/net/sf/cglib/delegator/TestDelegator.java
@@ -16,31 +16,32 @@ public class TestDelegator extends TestCase {
     
     public void testSimple() throws Exception {
         Object obj = Delegator.makeDelegator(new Class[]{ I1.class, I2.class },
-                                             new Object[]{ new C1(), new C2() });
+                                             new Object[]{ new C1(), new C2() },
+                                             null);
         assertTrue(((I1)obj).herby().equals("C1"));
         assertTrue(((I2)obj).derby().equals("C2"));
     }
 
     public void testDetermineInterfaces() throws Exception {
-        Object obj = Delegator.makeDelegator(new Object[]{ new C1(), new C2() });
+        Object obj = Delegator.makeDelegator(new Object[]{ new C1(), new C2() }, null);
         assertTrue(((I1)obj).herby().equals("C1"));
         assertTrue(((I2)obj).derby().equals("C2"));
     }
  
     public void testOverride() throws Exception {
-        Object obj = Delegator.makeDelegator(new Object[]{ new C1(), new C4() });
+        Object obj = Delegator.makeDelegator(new Object[]{ new C1(), new C4() }, null);
         assertTrue(((I1)obj).herby().equals("C1"));
         assertTrue(((I2)obj).derby().equals("C4"));
     }
 
     public void testNonOverride() throws Exception {
-        Object obj = Delegator.makeDelegator(new Object[]{ new C4(), new C1() });
+        Object obj = Delegator.makeDelegator(new Object[]{ new C4(), new C1() }, null);
         assertTrue(((I1)obj).herby().equals("C4"));
         assertTrue(((I2)obj).derby().equals("C4"));
     }
 
     public void testSubclass() throws Exception {
-        Object obj = Delegator.makeDelegator(new Object[]{ new C3B(), new C1() });
+        Object obj = Delegator.makeDelegator(new Object[]{ new C3B(), new C1() }, null);
         assertTrue(((I1)obj).herby().equals("C1"));
         assertTrue(((I2)obj).derby().equals("C2"));
         assertTrue(((I3)obj).extra().equals("C3B"));
