7554f2ce297a4b731234654e4e82744d51fee47e
Chris Nokleberg <chris@nokleberg.com>
2002-11-19 04:23:42 +0000

    decided the GenericKey* interfaces weren't useful enough to include in core pkg

--- src/proxy/net/sf/cglib/util/GenericKey2.java
+++
@@ -1,5 +0,0 @@
-package net.sf.cglib.util;
-
-public interface GenericKey2 {
-    public Object newInstance(Object o1, Object o2);
-}
--- src/proxy/net/sf/cglib/util/GenericKey4.java
+++
@@ -1,5 +0,0 @@
-package net.sf.cglib.util;
-
-public interface GenericKey4 {
-    public Object newInstance(Object o1, Object o2, Object o3, Object o4);
-}
