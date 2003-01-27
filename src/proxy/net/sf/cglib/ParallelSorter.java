c4ee4f1fb87b26b0f8f9217f20ef0f1278acb935
herbyderby <herbyderby>
2003-01-27 17:14:48 +0000

    Add licenses. Move setDebug implementation to CodeGeneratorBackend.

--- src/proxy/net/sf/cglib/ParallelSorter.java
+++ src/proxy/net/sf/cglib/ParallelSorter.java
@@ -1,7 +1,7 @@
 /*
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2002 The Apache Software Foundation.  All rights
+ * Copyright (c) 2003 The Apache Software Foundation.  All rights
  * reserved.
  *
  * Redistribution and use in source and binary forms, with or without
