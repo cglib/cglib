dad6fc5adea1cc0d65865ca29e6ed50b6c201df8
nemecec <nemecec>
2003-01-29 16:51:12 +0000

    renamed and split TestJdkCompatibleProxy to TestProxy and TestProxyJdk

--- src/test/net/sf/cglib/TestAll.java
+++ src/test/net/sf/cglib/TestAll.java
@@ -58,7 +58,7 @@ import junit.framework.*;
 /**
  *@author     Gerhard Froehlich <a href="mailto:g-froehlich@gmx.de">
  *      g-froehlich@gmx.de</a>
- *@version    $Id: TestAll.java,v 1.12 2003/01/24 00:31:26 herbyderby Exp $
+ *@version    $Id: TestAll.java,v 1.13 2003/01/29 16:51:31 nemecec Exp $
  */
 public class TestAll extends TestCase {
     public TestAll(String testName) {
@@ -75,7 +75,8 @@ public class TestAll extends TestCase {
         suite.addTest(TestMetaClass.suite());
         suite.addTest(TestDelegator.suite());
         suite.addTest(TestKeyFactory.suite());
-        suite.addTest(TestJdkCompatibleProxy.suite());
+        suite.addTest(TestProxy.suite());
+        suite.addTest(TestProxyJdk.suite());
         suite.addTest(TestMethodProxy.suite());
         suite.addTest(TestParallelSorter.suite());
            
