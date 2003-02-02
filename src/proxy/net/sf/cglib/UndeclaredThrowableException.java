c19b548c820c2d22ebda69b3f2e6e1d894c6e824
herbyderby <herbyderby>
2003-02-02 18:41:47 +0000

    Remove redundant code.

--- src/proxy/net/sf/cglib/UndeclaredThrowableException.java
+++ src/proxy/net/sf/cglib/UndeclaredThrowableException.java
@@ -55,26 +55,17 @@
 package net.sf.cglib;
 
 /**
- *
  * @author  baliuka
  */
 public class UndeclaredThrowableException extends CodeGenerationException {
-    private Throwable t;
-
     /**
      * Creates a new instance of <code>UndeclaredThrowableException</code> without detail message.
      */
     public UndeclaredThrowableException(Throwable t) {
         super(t);
-        this.t = t;
     }
     
     public Throwable getUndeclaredThrowable() {
-       return t;
-    }
-
-    // for jdk1.4 compatibility
-    public Throwable getCause() {
-        return t;
+        return getCause();
     }
 }
