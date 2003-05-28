c08b0c53c8b6c927112129e835b682d948f5de3e
herbyderby <herbyderby>
2003-05-28 03:56:44 +0000

    add multicasting bean delegation

---
+++ src/test/net/sf/cglib/DBean3.java
@@ -0,0 +1,13 @@
+package net.sf.cglib;
+
+class DBean3 {
+    private int age;
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
