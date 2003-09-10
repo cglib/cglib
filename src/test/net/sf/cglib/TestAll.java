8132206884d450c39b47d5b77cbd44bb1aeb06fb
herbyderby <herbyderby>
2003-09-10 02:23:04 +0000

    uncomment tests, again...

--- src/test/net/sf/cglib/TestAll.java
+++ src/test/net/sf/cglib/TestAll.java
@@ -60,7 +60,7 @@ import junit.framework.*;
 /**
  *@author     Gerhard Froehlich <a href="mailto:g-froehlich@gmx.de">
  *      g-froehlich@gmx.de</a>
- *@version    $Id: TestAll.java,v 1.29 2003/09/10 00:36:10 herbyderby Exp $
+ *@version    $Id: TestAll.java,v 1.30 2003/09/10 02:23:04 herbyderby Exp $
  */
 public class TestAll extends TestCase {
     public TestAll(String testName) {
@@ -73,21 +73,21 @@ public class TestAll extends TestCase {
         
         System.getProperties().list(System.out);
         TestSuite suite = new TestSuite();
-//         suite.addTest(TestEnhancer.suite());
+        suite.addTest(TestEnhancer.suite());
         suite.addTest(TestBulkBean.suite());
-//         suite.addTest(TestMixin.suite());
-//         suite.addTest(TestKeyFactory.suite());
-//         suite.addTest(TestProxy.suite());
-//         suite.addTest(TestMethodProxy.suite());
-//         suite.addTest(TestParallelSorter.suite());
-//         suite.addTest(TestSwitch.suite());
-//         suite.addTest(TestStringSwitch.suite());
-//         suite.addTest(TestBeanMap.suite());
-//         suite.addTest(TestDispatcher.suite());
-//         suite.addTest(TestLazyLoader.suite());
-//         suite.addTest(TestNoOp.suite());
-//         suite.addTest(TestMemberSwitch.suite());
-//         suite.addTest(TestFastClass.suite());
+        suite.addTest(TestMixin.suite());
+        suite.addTest(TestKeyFactory.suite());
+        suite.addTest(TestProxy.suite());
+        suite.addTest(TestMethodProxy.suite());
+        suite.addTest(TestParallelSorter.suite());
+        suite.addTest(TestSwitch.suite());
+        suite.addTest(TestStringSwitch.suite());
+        suite.addTest(TestBeanMap.suite());
+        suite.addTest(TestDispatcher.suite());
+        suite.addTest(TestLazyLoader.suite());
+        suite.addTest(TestNoOp.suite());
+        suite.addTest(TestMemberSwitch.suite());
+        suite.addTest(TestFastClass.suite());
         return suite;
     }
 
