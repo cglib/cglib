7c9c77c3924a6d0d5fbf5c26edcf0c314ff15d21
Chris Nokleberg <chris@nokleberg.com>
2003-09-21 20:49:37 +0000

    Move ExampleTransformer to AddPropertiesTransformer

--- src/proxy/net/sf/cglib/transform/AddPropertiesTransformer.java
+++ src/proxy/net/sf/cglib/transform/AddPropertiesTransformer.java
@@ -5,11 +5,11 @@ import net.sf.cglib.core.Signature;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Type;
 
-public class ExampleTransformer extends EmittingTransformer {
+public class AddPropertiesTransformer extends EmittingTransformer {
     private final String[] newPropertyNames;
     private final Class[] newPropertyTypes;
 
-    public ExampleTransformer(String[] newPropertyNames,
+    public AddPropertiesTransformer(String[] newPropertyNames,
                               Class[] newPropertyTypes) {
         this.newPropertyNames = newPropertyNames;
         this.newPropertyTypes = newPropertyTypes;
@@ -22,9 +22,9 @@ public class ExampleTransformer extends EmittingTransformer {
                     String name = newPropertyNames[i];
                     Type type = Type.getType(newPropertyTypes[i]);
                     String fieldName = "HIB$" + name;
-                    String methodName = TypeUtils.upperFirst(name);
                     declare_field(Constants.ACC_PRIVATE, fieldName, type, null);
 
+                    String methodName = TypeUtils.upperFirst(name);
                     begin_method(Constants.ACC_PUBLIC, new Signature("get" + methodName, type, new Type[0]), null);
                     load_this();
                     getfield(fieldName);
@@ -39,18 +39,5 @@ public class ExampleTransformer extends EmittingTransformer {
                 super.end_class();
             }
         };
-    }
-
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-        sb.append("ExampleTransformer{names=[");
-        for (int i = 0; i < newPropertyNames.length; i++) {
-            if (i > 0) {
-                sb.append(", ");
-            }
-            sb.append(newPropertyNames[i]);
-        }
-        sb.append("]}");
-        return sb.toString();
     }
 }
--- src/test/net/sf/cglib/transform/ExampleTask.java
+++ src/test/net/sf/cglib/transform/ExampleTask.java
@@ -2,7 +2,7 @@ package net.sf.cglib.transform;
 
 public class ExampleTask extends AbstractTransformTask {
     protected ClassTransformer getClassTransformer() {
-        return new ExampleTransformer(new String[]{ "chris" }, new Class[]{ Long.class });
+        return new AddPropertiesTransformer(new String[]{ "chris" }, new Class[]{ Long.class });
     }
 
     public boolean accept(String name) {
