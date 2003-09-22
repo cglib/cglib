92032112aed5ad61eadbf0732f10bf9626b97bee
Chris Nokleberg <chris@nokleberg.com>
2003-09-22 03:59:19 +0000

    add FieldProvider

--- src/test/net/sf/cglib/transform/Example.java
+++ src/test/net/sf/cglib/transform/Example.java
@@ -2,6 +2,7 @@ package net.sf.cglib.transform;
 
 public class Example {
     private int answer = 42;
+    private String word;
 
     public int getAnswer() {
         return answer;
@@ -9,5 +10,13 @@ public class Example {
 
     public void setAnswer(int answer) {
         this.answer = answer;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
     }
 }
