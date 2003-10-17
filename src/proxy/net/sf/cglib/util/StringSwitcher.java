6ea14051264685f6c3594367fdfbe9a87f50d260
Chris Nokleberg <chris@nokleberg.com>
2003-10-17 00:27:56 +0000

    Add docs

--- src/proxy/net/sf/cglib/util/StringSwitcher.java
+++ src/proxy/net/sf/cglib/util/StringSwitcher.java
@@ -59,6 +59,9 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.Type;
 
+/**
+ * This class implements a simple String->int mapping for a fixed set of keys.
+ */
 abstract public class StringSwitcher {
     private static final Type STRING_SWITCHER =
       TypeUtils.parseType("net.sf.cglib.util.StringSwitcher");
@@ -71,6 +74,15 @@ abstract public class StringSwitcher {
         public Object newInstance(String[] strings, int[] ints, boolean fixedInput);
     }
 
+    /**
+     * Helper method to create a StringSwitcher.
+     * For finer control over the generated instance, use a new instance of StringSwitcher.Generator
+     * instead of this static method.
+     * @param strings the array of String keys; must be the same length as the value array
+     * @param ints the array of integer results; must be the same length as the key array
+     * @param fixedInput if false, an unknown key will be returned from {@link #intValue} as <code>-1</code>; if true,
+     * the result will be undefined, and the resulting code will be faster
+     */
     public static StringSwitcher create(String[] strings, int[] ints, boolean fixedInput) {
         Generator gen = new Generator();
         gen.setStrings(strings);
@@ -82,6 +94,13 @@ abstract public class StringSwitcher {
     protected StringSwitcher() {
     }
 
+    /**
+     * Return the integer associated with the given key.
+     * @param s the key
+     * @return the associated integer value, or <code>-1</code> if the key is unknown (unless
+     * <code>fixedInput</code> was specified when this <code>StringSwitcher</code> was created,
+     * in which case the return value for an unknown key is undefined)
+     */
     abstract public int intValue(String s);
 
     public static class Generator extends AbstractClassGenerator {
@@ -95,14 +114,29 @@ abstract public class StringSwitcher {
             super(SOURCE);
         }
 
+        /**
+         * Set the array of recognized Strings.
+         * @param strings the array of String keys; must be the same length as the value array
+         * @see #setInts
+         */
         public void setStrings(String[] strings) {
             this.strings = strings;
         }
 
+        /**
+         * Set the array of integer results.
+         * @param ints the array of integer results; must be the same length as the key array
+         * @see #setStrings
+         */
         public void setInts(int[] ints) {
             this.ints = ints;
         }
 
+        /**
+         * Configure how unknown String keys will be handled.
+         * @param fixedInput if false, an unknown key will be returned from {@link #intValue} as <code>-1</code>; if true,
+         * the result will be undefined, and the resulting code will be faster
+         */
         public void setFixedInput(boolean fixedInput) {
             this.fixedInput = fixedInput;
         }
@@ -111,6 +145,9 @@ abstract public class StringSwitcher {
             return getClass().getClassLoader();
         }
 
+        /**
+         * Generate the <code>StringSwitcher</code>.
+         */
         public StringSwitcher create() {
             setNamePrefix(StringSwitcher.class.getName());
             Object key = KEY_FACTORY.newInstance(strings, ints, fixedInput);
