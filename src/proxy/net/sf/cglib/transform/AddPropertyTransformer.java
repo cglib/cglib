c268859a0f30017cf239b5c398000d6ced33c2f7
herbyderby <herbyderby>
2003-11-02 18:22:27 +0000

    refactor transform task to allow per-class transformers

--- src/proxy/net/sf/cglib/transform/AddPropertyTransformer.java
+++ src/proxy/net/sf/cglib/transform/AddPropertyTransformer.java
@@ -1,11 +1,21 @@
 package net.sf.cglib.transform;
 
+import java.util.*;
 import net.sf.cglib.core.*;
 import org.objectweb.asm.Type;
 
 public class AddPropertyTransformer extends EmittingTransformer {
     private final String[] names;
     private final Type[] types;
+
+    public AddPropertyTransformer(Map props) {
+        int size = props.size();
+        names = (String[])props.keySet().toArray(new String[size]);
+        types = new Type[size];
+        for (int i = 0; i < size; i++) {
+            types[i] = (Type)props.get(names[i]);
+        }
+    }
 
     public AddPropertyTransformer(String[] names, Type[] types) {
         this.names = names;
