4d62d46e902048c689b047c56caa1dc485582bc6
Chris Nokleberg <chris@nokleberg.com>
2007-02-07 23:56:05 +0000

    add protected getTag method to DefaultNamingPolicy

--- src/proxy/net/sf/cglib/core/DefaultNamingPolicy.java
+++ src/proxy/net/sf/cglib/core/DefaultNamingPolicy.java
@@ -31,28 +31,28 @@ public class DefaultNamingPolicy implements NamingPolicy {
     public static final DefaultNamingPolicy INSTANCE = new DefaultNamingPolicy();
     
     public String getClassName(String prefix, String source, Object key, Predicate names) {
-        
-        
-        StringBuffer sb = new StringBuffer();
-        sb.append( 
-                  (prefix != null) ? 
-                                     ( 
-                                      prefix.startsWith("java") ? 
-                                                   "$" + prefix : prefix 
-                                     )
-                                    : "net.sf.cglib.empty.Object"
-                 );
-        sb.append("$$");
-        sb.append(source.substring(source.lastIndexOf('.') + 1));
-        sb.append("ByCGLIB$$");
-        sb.append(Integer.toHexString(key.hashCode()));
-        String base = sb.toString();
+        if (prefix == null) {
+            prefix = "net.sf.cglib.empty.Object";
+        } else if (prefix.startsWith("java")) {
+            prefix = "$" + prefix;
+        }
+        String base =
+            prefix + "$$" + 
+            source.substring(source.lastIndexOf('.') + 1) +
+            getTag() + "$$" +
+            Integer.toHexString(key.hashCode());
         String attempt = base;
         int index = 2;
-        while (names.evaluate(attempt)) {
+        while (names.evaluate(attempt))
             attempt = base + "_" + index++;
-        }
-       
         return attempt;
+    }
+
+    /**
+     * Returns a string which is incorporated into every generated class name.
+     * By default returns "ByCGLIB"
+     */
+    protected String getTag() {
+        return "ByCGLIB";
     }
 }
