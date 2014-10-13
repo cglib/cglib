2d481af7c9944db59ed59a443f95edcc404b89d5
James Bodkin <JBodkin@users.noreply.github.com>
2014-10-13 11:02:09 +0100

    Remove code that was moved to integration tests project

--- cglib/src/test/java/net/sf/cglib/reflect/TestFastClass.java
+++ cglib/src/test/java/net/sf/cglib/reflect/TestFastClass.java
@@ -38,28 +38,6 @@ public class TestFastClass extends net.sf.cglib.CodeGenTestCase {
         FastClass.create(Simple.class).newInstance();
     }
     
-    // TODO: Re-enable
-    /*
-    public void testSignedClassProtectionDomain() throws Throwable {
-    	// SimpleClass must be signed for this test to work
-    	assertNotNull(SimpleClass.class.getProtectionDomain().getCodeSource());
-    	assertNotNull("SimpleClass.class must be signed for this test to work",SimpleClass.class.getProtectionDomain().getCodeSource().getCertificates());
-    	assertTrue("SimpleClass.class must be signed for this test to work", SimpleClass.class.getProtectionDomain().getCodeSource().getCertificates().length > 0);
-    	
-    	// when
-        Object instance = FastClass.create(SimpleClass.class).newInstance();
-        
-        // then
-    	assertNotNull(instance.getClass().getProtectionDomain().getCodeSource());
-    	assertNotNull(instance.getClass().getProtectionDomain().getCodeSource().getCertificates());
-    	assertTrue(instance.getClass().getProtectionDomain().getCodeSource().getCertificates().length > 0);
-    	assertEquals(SimpleClass.class.getProtectionDomain().getCodeSource().getCertificates().length, instance.getClass().getProtectionDomain().getCodeSource().getCertificates().length);
-    	for (int i = 0; i < SimpleClass.class.getProtectionDomain().getCodeSource().getCertificates().length; i++) {
-        	assertEquals(SimpleClass.class.getProtectionDomain().getCodeSource().getCertificates()[i], instance.getClass().getProtectionDomain().getCodeSource().getCertificates()[i]);
-		}
-    }
-    */
-
     public void testException() throws Throwable {
         FastClass fc = FastClass.create(ThrowsSomething.class);
         ThrowsSomething ts = new ThrowsSomething();
