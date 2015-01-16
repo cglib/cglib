5796b58d23f73ab3571a81c72778062ab9fa6433
unknown <rafael.winterhalter@R90BKBZB.bouvet.no>
2015-01-16 08:37:14 +0100

    Fixed bean copier that read getters instead of setters.

--- cglib/src/main/java/net/sf/cglib/beans/BeanCopier.java
+++ cglib/src/main/java/net/sf/cglib/beans/BeanCopier.java
@@ -109,7 +109,7 @@ abstract public class BeanCopier
             EmitUtils.null_constructor(ce);
             CodeEmitter e = ce.begin_method(Constants.ACC_PUBLIC, COPY, null);
             PropertyDescriptor[] getters = ReflectUtils.getBeanGetters(source);
-            PropertyDescriptor[] setters = ReflectUtils.getBeanGetters(target);
+            PropertyDescriptor[] setters = ReflectUtils.getBeanSetters(target);
 
             Map names = new HashMap();
             for (int i = 0; i < getters.length; i++) {
